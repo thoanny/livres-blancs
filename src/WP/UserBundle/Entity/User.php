81f3b861cd03746bcefd3bcc14ee9c9a61a75004
Anthony Destenay <anthony.destenay@gmail.com>
2015-02-20 12:47:42 +0100

    Configuration de base pour la sécurité

---
+++ src/WP/UserBundle/Entity/User.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace WP\UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * User
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="WP\UserBundle\Entity\UserRepository")
+ */
+class User extends BaseUser
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+}
---
+++ src/WP/UserBundle/WPUserBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace WP\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class WPUserBundle extends Bundle
+{
+    public function getParent()
+    {
+        return 'FOSUserBundle';
+    }
+}
