f26fbf0f67c89ac9120c426b5875c4ddae2c7581
Anthony Destenay <anthony.destenay@gmail.com>
2015-03-13 16:44:04 +0100

    Ajout de la migration doctrine au Kernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -24,6 +24,7 @@ class AppKernel extends Kernel
             new WP\WhitepaperBundle\WPWhitepaperBundle(),
             new \JMS\SerializerBundle\JMSSerializerBundle(),
             new \FOS\ElasticaBundle\FOSElasticaBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
